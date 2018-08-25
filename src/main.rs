555f1bb681ce3512e354dd163401a9cf1059c976
Yusuke Nojima <nojima@ynojima.com>
2018-08-25 23:30:36 +0900

    Implement basic TCP server

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
 extern crate serde;
 extern crate serde_json;
-
 #[macro_use]
 extern crate serde_derive;
+#[macro_use]
+extern crate log;
+extern crate env_logger;
 
 mod wal;
+mod server;
 
 use std::error::Error;
 use std::fs::File;
+use std::net::SocketAddr;
+
+fn setup_logger() {
+    let env = env_logger::Env::default()
+        .filter_or(env_logger::DEFAULT_FILTER_ENV, "info");
+    env_logger::Builder::from_env(env).init();
+}
 
 fn main() -> Result<(), Box<Error>> {
+    setup_logger();
+
     let entry1 = wal::WalEntry::Write {
         transaction_id: 100,
         key: "hello".to_string(),
@@ -24,6 +36,10 @@ fn main() -> Result<(), Box<Error>> {
     let mut wal_writer = wal::WalWriter::new(file);
     wal_writer.write(&entry1)?;
     wal_writer.write(&entry2)?;
+
+    let addr: SocketAddr = "0.0.0.0:5555".parse()?;
+    let s = server::Server::new(addr);
+    s.listen_and_serve()?;
 
     Ok(())
 }
---
+++ src/server.rs
@@ -0,0 +1,37 @@
+use std::io::Read;
+use std::net::{TcpListener, TcpStream, SocketAddr};
+use std::error::Error;
+
+pub struct Server {
+    addr: SocketAddr
+}
+
+impl Server {
+    pub fn new(addr: SocketAddr) -> Self {
+        Self { addr }
+    }
+
+    pub fn listen_and_serve(&self) -> Result<(), Box<Error>> {
+        let listener = TcpListener::bind(self.addr)?;
+        info!("Server start: addr={}", self.addr);
+
+        for stream in listener.incoming() {
+            if let Err(e) = self.handle_stream(stream?) {
+                error!("Failed to handle connection: err={}", e);
+            }
+        }
+
+        Ok(())
+    }
+
+    fn handle_stream(&self, mut stream: TcpStream) -> Result<(), Box<Error>> {
+        info!("Connected: peer={}", stream.peer_addr()?);
+
+        let mut buffer = String::new();
+        stream.read_to_string(&mut buffer)?;
+
+        info!("Client sent: {}", buffer);
+
+        Ok(())
+    }
+}
