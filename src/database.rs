b741db4674e18e1fac289a1b8fa545dbb8e20075
Yusuke Nojima <nojima@ynojima.com>
2018-08-26 19:39:09 +0900

    Add Transaction struct

--- src/database.rs
+++ src/database.rs
@@ -6,6 +6,7 @@ use wal;
 pub struct Database {
     map: BTreeMap<String, String>,
     wal: wal::Wal,
+    next_transaction_id: u64,
 }
 
 impl Database {
@@ -13,21 +14,39 @@ impl Database {
         Ok(Self {
             map: BTreeMap::new(),
             wal: wal::Wal::open(wal_path)?,
+            next_transaction_id: 1,
         })
     }
 
+    pub fn begin(&mut self) -> Transaction {
+        let transaction_id = self.next_transaction_id;
+        self.next_transaction_id += 1;
+
+        Transaction {
+            transaction_id,
+            database: self,
+        }
+    }
+}
+
+pub struct Transaction<'a> {
+    transaction_id: u64,
+    database: &'a mut Database,
+}
+
+impl<'a> Transaction<'a> {
     pub fn read(&mut self, key: &str) -> Option<String> {
-        self.map.get(key).map(|value| value.to_string())
+        self.database.map.get(key).map(|value| value.to_string())
     }
 
     pub fn write(&mut self, key: &str, value: &str) -> Result<(), Box<Error>> {
-        self.wal.write(&wal::WalEntry::Write {
-            transaction_id: 1,
+        self.database.wal.write(&wal::WalEntry::Write {
+            transaction_id: self.transaction_id,
             key: key.to_string(),
             value: value.to_string(),
         })?;
 
-        self.map.insert(key.to_string(), value.to_string());
+        self.database.map.insert(key.to_string(), value.to_string());
 
         Ok(())
     }
--- src/server.rs
+++ src/server.rs
@@ -1,6 +1,7 @@
 use command::parse;
 use command::Command;
 use database::Database;
+use database::Transaction;
 use std::error::Error;
 use std::io::{BufRead, BufReader, BufWriter, Write};
 use std::net::{SocketAddr, TcpListener, TcpStream};
@@ -34,12 +35,14 @@ impl Server {
         let reader = BufReader::new(stream.try_clone()?);
         let mut writer = BufWriter::new(stream);
 
+        let mut transaction = self.database.begin();
+
         for opt_line in reader.lines() {
             let line = opt_line?;
             match parse(&line) {
                 Ok(command) => {
                     info!("Command: {:?}", command);
-                    self.handle_command(&command, &mut writer)?;
+                    handle_command(&command, &mut transaction, &mut writer)?;
                 }
                 Err(e) => {
                     info!("{}", e);
@@ -51,37 +54,37 @@ impl Server {
 
         Ok(())
     }
+}
 
-    fn handle_command(
-        &mut self,
-        command: &Command,
-        writer: &mut BufWriter<TcpStream>,
-    ) -> Result<(), Box<Error>> {
-        match command {
-            Command::Write { key, value } => {
-                self.database.write(key, value)?;
-                write!(writer, "OK\n")?;
-                writer.flush()?;
-                Ok(())
-            }
-            Command::Read { key } => {
-                match self.database.read(key) {
-                    Some(value) => write!(writer, "OK {}\n", value)?,
-                    None => write!(writer, "NOT_FOUND\n")?,
-                };
-                writer.flush()?;
-                Ok(())
-            }
-            Command::Commit => {
-                write!(writer, "NOT_IMPLEMENTED\n")?;
-                writer.flush()?;
-                Ok(())
-            }
-            Command::Rollback => {
-                write!(writer, "NOT_IMPLEMENTED\n")?;
-                writer.flush()?;
-                Ok(())
-            }
+fn handle_command(
+    command: &Command,
+    transaction: &mut Transaction,
+    writer: &mut BufWriter<TcpStream>,
+) -> Result<(), Box<Error>> {
+    match command {
+        Command::Write { key, value } => {
+            transaction.write(key, value)?;
+            write!(writer, "OK\n")?;
+            writer.flush()?;
+            Ok(())
+        }
+        Command::Read { key } => {
+            match transaction.read(key) {
+                Some(value) => write!(writer, "OK {}\n", value)?,
+                None => write!(writer, "NOT_FOUND\n")?,
+            };
+            writer.flush()?;
+            Ok(())
+        }
+        Command::Commit => {
+            write!(writer, "NOT_IMPLEMENTED\n")?;
+            writer.flush()?;
+            Ok(())
+        }
+        Command::Rollback => {
+            write!(writer, "NOT_IMPLEMENTED\n")?;
+            writer.flush()?;
+            Ok(())
         }
     }
 }
