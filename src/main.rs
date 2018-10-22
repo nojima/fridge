bf5281b660b07be18986747a4533bd2707086393
Yusuke Nojima <nojima@ynojima.com>
2018-10-22 19:32:23 +0900

    Implement check sum of WAL record

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,12 @@ mod protos;
 mod server;
 mod wal;
 
-#[macro_use] extern crate log;
-#[macro_use] extern crate serde_derive;
+#[macro_use]
+extern crate log;
+#[macro_use]
+extern crate serde_derive;
 extern crate byteorder;
+extern crate crc;
 extern crate env_logger;
 extern crate protobuf;
 extern crate serde;
--- src/wal.rs
+++ src/wal.rs
@@ -1,10 +1,13 @@
 use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
 use command::Command;
+use crc::{crc64, Hasher64};
 use protobuf::Message;
 use protos::wal as proto;
 use std::error::Error;
+use std::fmt;
 use std::fs::File;
 use std::fs::OpenOptions;
+use std::io;
 use std::io::ErrorKind;
 use std::io::{BufReader, Read, Write};
 use std::path::Path;
@@ -47,11 +50,18 @@ impl WalWriter {
         }
 
         let mut buffer = vec![];
+        let mut digest = Crc64Digest::new();
+
+        let record_size = record.compute_size();
+        buffer.write_u32::<BigEndian>(record_size)?;
+        digest.write_u32::<BigEndian>(record_size)?;
+
         record.write_to_writer(&mut buffer)?;
+        record.write_to_writer(&mut digest)?;
 
-        self.file.write_u32::<BigEndian>(buffer.len() as u32)?;
+        buffer.write_u64::<BigEndian>(digest.sum())?;
+
         self.file.write(&buffer[..])?;
-
         self.file.sync_data()?;
         Ok(())
     }
@@ -72,6 +82,8 @@ impl WalReader {
     }
 
     pub fn read(&mut self) -> Result<(Option<WalEntry>, u64), Box<Error>> {
+        let mut digest = Crc64Digest::new();
+
         let record_len = match self.reader.read_u32::<BigEndian>() {
             Ok(n) => n,
             Err(ref err) if err.kind() == ErrorKind::UnexpectedEof => {
@@ -81,11 +93,35 @@ impl WalReader {
                 return Err(Box::new(err));
             }
         };
+        digest.write_u32::<BigEndian>(record_len)?;
         self.position += 4;
 
         let mut buffer = vec![0; record_len as usize];
-        self.reader.read_exact(&mut buffer[..])?;
+        match self.reader.read_exact(&mut buffer[..]) {
+            Ok(_) => {}
+            Err(ref err) if err.kind() == ErrorKind::UnexpectedEof => {
+                return Err(Box::new(IncompleteWalRecordError {}));
+            }
+            Err(err) => {
+                return Err(Box::new(err));
+            }
+        }
+        digest.write(&buffer[..])?;
         self.position += record_len as u64;
+
+        let sum = self.reader.read_u64::<BigEndian>()?;
+        self.position += 8;
+
+        // Check sum
+        if digest.sum() != sum {
+            error!(
+                "Invalid check sum: position={}, recorded={}, calculated={}",
+                self.position,
+                sum,
+                digest.sum()
+            );
+            return Err(Box::new(IncompleteWalRecordError {}));
+        }
 
         let mut record = proto::WalRecord::new();
         record.merge_from_bytes(&buffer[..])?;
@@ -114,6 +150,55 @@ impl WalReader {
     pub fn truncate(&mut self, length: u64) -> Result<(), Box<Error>> {
         let file = self.reader.get_mut();
         file.set_len(length)?;
+        Ok(())
+    }
+}
+
+// An error for partially written WAL record.
+// When this error occurrs, we should trancate WAL.
+#[derive(Debug, Clone)]
+struct IncompleteWalRecordError {}
+
+impl fmt::Display for IncompleteWalRecordError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "incomplete WAL record")
+    }
+}
+
+impl Error for IncompleteWalRecordError {
+    fn description(&self) -> &str {
+        "incomplete WAL record"
+    }
+
+    fn cause(&self) -> Option<&Error> {
+        None
+    }
+}
+
+// Wrap crc64::Digest in order to implement io::Write.
+struct Crc64Digest {
+    d: crc64::Digest,
+}
+
+impl Crc64Digest {
+    fn new() -> Self {
+        Crc64Digest {
+            d: crc64::Digest::new(crc64::ECMA),
+        }
+    }
+
+    fn sum(&self) -> u64 {
+        self.d.sum64()
+    }
+}
+
+impl io::Write for Crc64Digest {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        self.d.write(buf);
+        Ok(buf.len())
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
         Ok(())
     }
 }
